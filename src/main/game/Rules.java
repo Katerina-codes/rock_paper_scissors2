483e38ede52dd7572ca89d21fbd68f7cff7c8bbe
Katerina <katerinaldg@gmail.com>
2017-11-30 15:49:47 +0000

    Extracted method which finds score for rock

--- src/main/game/Rules.java
+++ src/main/game/Rules.java
@@ -4,14 +4,22 @@ public class Rules {
     public String scoreGame(String playerOneMove, String playerTwoMove) {
         if (playerOneMove.equals(playerTwoMove)) {
             return "draw";
-        } else if (playerOneMove.equals("rock") && (playerTwoMove.equals("scissors"))) {
-            return "rock wins";
+        } else if (playerOneMove.equals("rock")) {
+            return findScoreForRock(playerTwoMove);
         } else if (playerOneMove.equals("scissors") && (playerTwoMove.equals("rock"))) {
             return "rock wins";
         } else if (playerOneMove.equals("rock") && (playerTwoMove.equals("paper"))) {
             return "paper wins";
         } else {
             return "scissors wins";
+        }
+    }
+
+    public String findScoreForRock(String playerTwoMove) {
+        if (playerTwoMove.equals("scissors")) {
+            return "rock wins";
+        } else {
+            return "paper wins";
         }
     }
 }
