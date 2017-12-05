0606d716f305791e50eadfffbdcdd7d5ec2e39f0
Katerina <katerinaldg@gmail.com>
2017-12-05 15:26:48 +0000

    Added conversion methods
    - Added announceWinnerTwo method.

--- src/main/game/CommandlineUi.java
+++ src/main/game/CommandlineUi.java
@@ -1,6 +1,7 @@
 package main.game;
 
 import java.io.*;
+import java.util.HashMap;
 
 public class CommandlineUi implements Ui {
 
@@ -26,7 +27,42 @@ public class CommandlineUi implements Ui {
         return userMove;
     }
 
-    public void announceWinner(String winningMove) {
-         output.println(String.format("%s wins!", winningMove));
+    public Moves getsMoveTwo() {
+        String userMove = null;
+        try {
+            userMove = input.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Moves convertedUserMove = convertMove(userMove);
+        return convertedUserMove;
     }
+
+    public Moves convertMove(String userMove) {
+        HashMap<String, Moves> convertedMove = new HashMap<>();
+        convertedMove.put("rock", Moves.ROCK);
+        convertedMove.put("paper", Moves.PAPER);
+        convertedMove.put("scissors", Moves.SCISSORS);
+
+        return convertedMove.get(userMove);
+    }
+
+    public String convertWinningMove(Result winningMove) {
+        HashMap<Result, String> convertedResult = new HashMap<>();
+        convertedResult.put(Result.PLAYER_ONE_WINS, "Player one wins!");
+        convertedResult.put(Result.PLAYER_TWO_WINS, "Player two wins!");
+        convertedResult.put(Result.DRAW, "It's a draw!");
+
+        return convertedResult.get(winningMove);
+    }
+
+    public void announceWinner(String winningMove) {
+        output.println(String.format("%s wins!", winningMove));
+    }
+
+    public void announceWinnerTwo(Result winningMove) {
+        String convertedWinningMove = convertWinningMove(winningMove);
+        output.println(String.format("%s wins!", convertedWinningMove));
+    }
+
 }
