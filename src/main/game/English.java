c6c45aa8d09ec9ac0677adc3a410a8d335b97ba1
Katerina <katerinaldg@gmail.com>
2017-12-11 13:48:46 +0000

    English implements language interface

--- src/main/game/English.java
+++ src/main/game/English.java
@@ -1,6 +1,6 @@
 package main.game;
 
-public class English {
+public class English implements Language {
     public String promptForMove() {
         return "Pick a move. Enter 'rock', 'paper' or 'scissors': ";
     }
