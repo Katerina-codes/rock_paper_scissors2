ad82c844089e8a34cfe94a3fff9aa7dd2c25e304
Katerina <katerinaldg@gmail.com>
2017-12-12 21:19:20 +0000

    Add all tests for English

--- src/test/game/Language/EnglishTest.java
+++ src/test/game/Language/EnglishTest.java
@@ -4,6 +4,7 @@ import main.game.Language.English;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class EnglishTest {
 
@@ -12,5 +13,19 @@ public class EnglishTest {
         English english = new English();
 
         assertEquals("Pick a move. Enter 'rock', 'paper' or 'scissors': ", english.promptForMove());
+    }
+
+    @Test
+    public void announceWinner() {
+        English english = new English();
+
+        assertEquals("Player One wins!", english.announceWinner("Player One"));
+    }
+
+    @Test
+    public void askForLanguage() {
+        English english = new English();
+
+        assertTrue(english.askForLanguage().contains("Enter '1' for English"));
     }
 }
