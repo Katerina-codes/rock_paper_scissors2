2ef8c1b4c899804346c2928b9170f7181a25d327
Katerina <katerinaldg@gmail.com>
2017-12-14 15:05:09 +0000

    Fix bug. Draw is correctly announced in greek

--- src/main/game/Language/Greek.java
+++ src/main/game/Language/Greek.java
@@ -8,10 +8,12 @@ public class Greek implements Language {
 
     public String announceWinner(String convertedWinningMove) {
         if (convertedWinningMove.contains("One")) {
-            convertedWinningMove = "Paíktis ένα";;
-    } else {
-        convertedWinningMove = "Paíktis dýo";;
-    }
+            convertedWinningMove = "Paíktis ένα";
+        } else if (convertedWinningMove.contains("draw")) {
+            return "Είναι ισοπαλία!";
+        } else {
+            convertedWinningMove = "Paíktis dýo";
+        }
         return (String.format("%s κερδίζει!", convertedWinningMove));
     }
 
