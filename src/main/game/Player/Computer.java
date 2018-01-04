8a7c01793909e758cf1854d79d66e1e5a417f484
Katerina <katerinaldg@gmail.com>
2018-01-04 13:37:59 +0000

    Create playMoveTwo method that returns string

--- src/main/game/Player/Computer.java
+++ src/main/game/Player/Computer.java
@@ -2,9 +2,18 @@ package main.game.Player;
 
 import main.game.Moves.Moves;
 
+import java.util.Random;
+
 public class Computer {
 
     public static Moves playMove() {
         return Moves.getRandomMove();
+    }
+
+    public String playMoveTwo() {
+        String[] moves = {"rock", "paper", "scissors"};
+        Random random = new Random();
+        int randomMove = random.nextInt(moves.length);
+        return moves[randomMove];
     }
 }
