bc2cdc33227ad4f94f538fb37b27e568a3695d20
Katerina <katerinaldg@gmail.com>
2018-01-17 14:13:42 +0000

    Dry up tests

--- src/test/game/RulesTest.java
+++ src/test/game/RulesTest.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.assertEquals;
 public class RulesTest {
 
     private Map moves;
+    private Rules rules;
 
     @Before
     public void setUp() {
@@ -29,51 +30,39 @@ public class RulesTest {
         moves.put(ROCK, rockMove);
         moves.put(PAPER, paperMove);
         moves.put(SCISSORS, scissorsMove);
+        rules = new Rules(moves);
     }
 
     @Test
     public void scoresADraw() {
-
-        Rules rules = new Rules(moves);
-
         assertEquals(DRAW, rules.findWinningPLayer(ROCK, ROCK));
     }
 
     @Test
     public void scoresAWinForRockAgainstScissors() {
-        Rules rules = new Rules(moves);
-
         assertEquals(ROCK, rules.scoreMove(ROCK, SCISSORS));
         assertEquals(ROCK, rules.scoreMove(SCISSORS, ROCK));
     }
 
     @Test
     public void scoresLossForRockAgainstPaper() {
-        Rules rules = new Rules(moves);
-
         assertEquals(PAPER, rules.scoreMove(ROCK, PAPER));
         assertEquals(PAPER, rules.scoreMove(PAPER, ROCK));
     }
 
     @Test
     public void scoresLossForPaperAgainstScissors() {
-        Rules rules = new Rules(moves);
-
         assertEquals(SCISSORS, rules.scoreMove(SCISSORS, PAPER));
         assertEquals(SCISSORS, rules.scoreMove(PAPER, SCISSORS));
     }
 
     @Test
     public void playerOneWinsWhenPlayerOneIsScissorsAndWinningMoveIsScissors() {
-        Rules rules = new Rules(moves);
-
         assertEquals(Result.PLAYER_ONE_WINS, rules.scoreGame(SCISSORS, SCISSORS));
     }
 
     @Test
     public void playerOneWinsWhenPlayerTwoIsScissorsAndWinningMoveIsScissors() {
-        Rules rules = new Rules(moves);
-
         assertEquals(Result.PLAYER_TWO_WINS, rules.scoreGame(PAPER, SCISSORS));
     }
 }
